89c850c5f14881936a044e18e3c65ec8eff96f98
sudhakarsn15 <35915646+sudhakarsn15@users.noreply.github.com>
2021-12-21 22:29:37 +0530

    udpated

--- src/App.js
+++ src/App.js
@@ -1,18 +1,31 @@
 import "./styles.css";
-
+import moment from "moment-timezone";
+import tzlookup from "tz-lookup";
 export default function App() {
   return (
     <div className="App">
       <h1>Time Zone Libraries</h1>
-      <MomentJS />
+      <MomentJSFnc />
+      <TzlookupFnc />
     </div>
   );
 }
 
-const MomentJS = () => {
+const MomentJSFnc = () => {
+  var jun = moment("2014-06-01T12:00:00Z");
   return (
     <>
       <h3>momentJS</h3>
+      <div>{jun.tz("America/Los_Angeles").format("ha z")}</div>
+    </>
+  );
+};
+
+const TzlookupFnc = () => {
+  return (
+    <>
+      <h3>tzlookup</h3>
+      <div>{tzlookup(42.7235, -73.6931)}</div>
     </>
   );
 };
