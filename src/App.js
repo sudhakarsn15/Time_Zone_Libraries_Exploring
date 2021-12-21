80235ddcb71b3abb2b88463d1e8df287ed5512d3
sudhakarsn15 <35915646+sudhakarsn15@users.noreply.github.com>
2021-12-21 20:57:31 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import "./styles.css";
+
+export default function App() {
+  return (
+    <div className="App">
+      <h1>Time Zone Libraries</h1>
+      <MomentJS />
+    </div>
+  );
+}
+
+const MomentJS = () => {
+  return (
+    <>
+      <h3>momentJS</h3>
+    </>
+  );
+};
